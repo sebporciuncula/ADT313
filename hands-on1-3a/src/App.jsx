7a569a14ebb2ea24d1e104422fc08804d1d1e5dc
Sebastian Sonny P. Porciuncula <143860104+sebporciuncula@users.noreply.github.com>
2024-08-31 22:09:39 +0800

    Add files via upload
    hands-on1-3a

---
+++ hands-on1-3a/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+import Name from './component/Name/Name';
+import Section from './component/Section/Section';
+import Description from './component/Description/Description';
+
+
+function App(){
+  const userInformation = {
+firstName: 'Sebastian Sonny ',
+lastName: 'Porciuncula',
+Section: "BSIT-3A",
+Description:"Eyyable sana this semester"
+
+  }
+
+  return (
+    <div className="App"> 
+    <Name firstName={userInformation.firstName}lastName={userInformation.lastName}/>
+    <Section Section={userInformation.Section}/>
+    <Description Description={userInformation.Description}/>
+
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ hands-on1-3a/src/component/User/User.jsx
@@ -0,0 +1,12 @@
+import './User.css';
+function User() {
+return(
+<div>
+     <h1> Sebastian Sonny Porciuncula</h1>
+     <h2>Bsit-3A</h2>
+     <p>Eyyable sana this semester</p>
+</div>
+)
+}
+
+export default User;
